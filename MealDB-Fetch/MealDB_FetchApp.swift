d2d9dd4746e7f50a17f0a665a038ba6fc6e479fb
burhanj888 <burhanj888@gmail.com>
2024-06-26 19:45:28 -0400

    Initial Commit

---
+++ MealDB-Fetch/MealDB_FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDB_FetchApp.swift
+//  MealDB-Fetch
+//
+//  Created by Burhanuddin Jinwala on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MealDB_FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
